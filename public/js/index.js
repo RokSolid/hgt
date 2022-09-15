3d8f00b3460b217e162c409a2dff2c01e0bc6419
Shahab Ali <code.door03@gmail.com>
2022-09-15 12:05:51 +0500

    final commit

--- public/js/index.js
+++ public/js/index.js
@@ -1,45 +1,58 @@
+// aos
 window.addEventListener('load', () => {
-    AOS.init({
-      duration: 1000,
-      easing: "ease-in-out",
-      once: true,
-      mirror: false
-    });
+  AOS.init({
+    duration: 1000,
+    easing: "ease-in-out",
+    once: true,
+    mirror: false
   });
-
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
 });
 
-
+// smoth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
 
 // counter 
-let endDateElm = "10 April 2023 12:01 am"
 let countDownItem = Array.from(document.querySelectorAll('.count_down'))
+var timeInSecs;
+var ticker;
 
-function countDown() {
-    let endDate = new Date(endDateElm);
-    let newDate = new Date();
-    let dateDiff = (endDate - newDate) / 1000
-    if (dateDiff > 0) {
-        let day = Math.floor(dateDiff / 3600 / 24)
-        let hour = Math.floor(dateDiff / 3600) % 24
-        let min = Math.floor(dateDiff / 60) % 60
-        let sec = Math.floor(dateDiff % 60)
-        countDownItem[0].textContent = day
-        countDownItem[1].textContent = hour
-        countDownItem[2].textContent = min
-        countDownItem[3].textContent = sec
-    } else {
-        clearInterval(stop)
-    }
+function startTimer(secs) {
+  timeInSecs = parseInt(secs);
+  ticker = setInterval("tick()", 1000);
 }
-let stop = setInterval(() => {
-    countDown();
-}, 1000);
+
+function tick() {
+  var secs = timeInSecs;
+  if (secs > 0) {
+    timeInSecs--;
+  }
+  else {
+    clearInterval(ticker);
+    startTimer(86400 * 5, 3600 * 5, 60 * 5);
+  }
+
+  var days = Math.floor(secs / 86400);
+  secs %= 86400;
+  var hours = Math.floor(secs / 3600);
+  secs %= 3600;
+  var mins = Math.floor(secs / 60);
+  secs %= 60;
+  var day = ((days < 10) ? "0" : "") + days;
+  var hour = ((hours < 10) ? "0" : "") + hours;
+  var min = ((mins < 10) ? "0" : "") + mins;
+  var sec = ((secs < 10) ? "0" : "") + secs
+  countDownItem[0].textContent = day
+  countDownItem[1].textContent = hour
+  countDownItem[2].textContent = min
+  countDownItem[3].textContent = sec
+}
+
+startTimer(86400 * 5, 3600 * 5, 60 * 5);
 
